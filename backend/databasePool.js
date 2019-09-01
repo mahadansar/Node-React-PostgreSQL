02ecdfd50e2eca554a66fc1efd8fb3276229e5e6
Mahad Ansar <46062897+mahadansar@users.noreply.github.com>
2019-09-01 22:52:48 +0500

    working 1

--- backend/databasePool.js
+++ backend/databasePool.js
@@ -1,4 +1,6 @@
 const { Pool } = require('pg')
+const databaseConfiguration = require('./secrets/databaseConfiguration')
 
-const pool = new Pool()
+const pool = new Pool(databaseConfiguration)
 
+module.exports = pool
