3f7543ad79c1c184a2a2c7c388cc4a25c8d3138d
Mahad Ansar <46062897+mahadansar@users.noreply.github.com>
2019-09-02 00:33:04 +0500

    dragontraits

--- backend/bin/configure_db.sh
+++ backend/bin/configure_db.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-export PGPASSWORD = 'node_password'
-
 echo "Configuring Database"
 
-dropdb -U node_user dragonstackdb 
-createdb -U node_user dragonstackdb
+psql
+
+DROP DATABASE dragonstackdb;
+CREATE DATABASE dragonstackdb;
 
 psql -U node_user dragonstackdb < .bin/sql/generation.sql
 psql -U node_user dragonstackdb < .bin/sql/dragon.sql
 psql -U node_user dragonstackdb < .bin/sql/trait.sql
+psql -U node_user dragonstackdb < .bin/sql/dragonTrait.sql
 
 node ./bin/insertTraits.js
 
---
+++ backend/bin/sql/dragonTrait.sql
@@ -0,0 +1,1 @@
+CREATE TABLE dragonTrait("traitId" INTEGER, "dragonId" INTEGER, FOREIGN KEY ("traitId") REFERENCES trait(id), FOREIGN KEY ("dragonId") REFERENCES dragon(id) );
