ce06d2e07e5f3aac84f90dabcb14f080bc116322
Mahad Ansar <46062897+mahadansar@users.noreply.github.com>
2019-09-01 16:42:15 +0500

    test 2

---
+++ backend/config.js
@@ -0,0 +1,14 @@
+const SECONDS = 1000
+const MINUTES = SECONDS * 60
+const HOURS = MINUTES * 60
+const DAYS = Hours * 24
+
+const REFRESH_RATE = 5
+
+module.exports {
+    SECONDS,
+    MINUTES,
+    HOURS,
+    DAYS,
+    REFRESH_RATE
+}
--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,20 @@ const Dragon = require('./dragon')
 
 const smoag = new Dragon({
     nickname: 'Smog' ,
-    birthdate: new Date()
+    birthdate: new Date(),
+    traits: [
+        {traitType: 'backgroundColor', traitValue: 'green'}
+    ]
 })
 
 setTimeout( () => {
     const aaa = new Dragon()
     console.log(aaa)
+}, 3000 )
+
+setTimeout( () => {
+    const aa = new Dragon()
+    console.log(aa)
 }, 3000 )
 
 console.log(smoag)
