85686b9872c58365511e3004f6d6112bc4075851
Mahad Ansar <46062897+mahadansar@users.noreply.github.com>
2019-09-01 23:08:59 +0500

    z

--- backend/app/generation/table.js
+++ backend/app/generation/table.js
@@ -2,10 +2,20 @@ const pool = require('../../databasePool')
 
 class GenerationTable {
     static storeGeneration(generation) {
-        pool.query('INSERT INTO generation(expiration) VALUES($1)',
-        [generation.expiration],
-        (error, response) => {
-            if (error) return console.error(error)
+
+        return new Promise((resolve, reject) => {
+
+            pool.query('INSERT INTO generation(expiration) VALUES($1) RETURNING id',
+            [generation.expiration],
+            (error, response) => {
+                if (error) return console.error(error)
+    
+                const generationId = response.rows[0].id
+
+                resolve({ generationId })
+            }
+            )
+
         }
         )
     }
