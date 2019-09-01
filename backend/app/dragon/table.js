efdf32cfee9526998b16b37215eeb10528a88208
Mahad Ansar <46062897+mahadansar@users.noreply.github.com>
2019-09-02 00:53:14 +0500

    sad

--- backend/app/dragon/table.js
+++ backend/app/dragon/table.js
@@ -1,4 +1,5 @@
 const pool = require('../../databasePool')
+const DragonTraitTable = require('../dragonTrait/table')
 
 class DragonTable{
     static storeDragon(dragon) {
@@ -14,6 +15,14 @@ class DragonTable{
                     if(error) return reject(error)
 
                     const dragonId = response.rows[0].id
+
+                    Promise.all(dragon.traits.map(({traitType, traitValue}) => {
+                        return DragonTraitTable.storeDragonTrait({
+                            dragonId, traitType, traitValue
+                        })
+                    }))
+                     .then(() => resolve ({ dragonId }))
+                     .catch(error => reject(error))
 
                     resolve ({ dragonId })
                 }
