77c7d63371d6c7defa79ec6320ffe2e002f18d68
Mahad Ansar <46062897+mahadansar@users.noreply.github.com>
2019-09-01 23:18:08 +0500

    generation id's worked on --working

--- backend/app/generation/engine.js
+++ backend/app/generation/engine.js
@@ -16,14 +16,22 @@ class GenerationEngine{
     }
 
     buildNewGeneration(){
-        this.generation = new Generation()
+        const generation = new Generation()
 
-        GenerationTable.storeGeneration(this.generation)
+        GenerationTable.storeGeneration(generation)
+        .then(({ generationId }) => {
+            this.generation = generation
 
-        this.timer = setTimeout(
+            this.generation.generationId = generationId
+
+            console.log('new generation', this.generation)
+
+            this.timer = setTimeout(
             () => this.buildNewGeneration(), 
             this.generation.expiration.getTime() - Date.now()
         )
+        })
+        .catch(error => console.error(error))
     }
 }
 
