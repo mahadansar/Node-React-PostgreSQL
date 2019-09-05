2e7d77f21f63b97aeb384c533f9d63c8b171e6dd
Mahad Ansar <46062897+mahadansar@users.noreply.github.com>
2019-09-05 20:50:40 +0500

    fixed code with redux reducers with all states,

--- frontend/src/components/Generation.js
+++ frontend/src/components/Generation.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-
-const DEFAULT_GENERATION = { generationId: '', expiration: '' }
+import { fetchGeneration } from '../actions/generation'
 
 const MINIMUM_DELAY = 3000
 
 class Generation extends Component{
-    state = { generation: DEFAULT_GENERATION }
-
     timer = null
 
     componentDidMount() {
@@ -18,21 +15,10 @@ class Generation extends Component{
         clearTimeout(this.timer)
     }
 
-
-    fetchGeneration = () => {
-        fetch('http://localhost:5000/generation')
-         .then(response => response.json())
-          .then(json => {console.log('json', json)
-        
-          this.setState({ generation: json.generation })
-        })
-          .catch(error => console.error('error', error))
-    }
-
     fetchNextGeneration = () => { 
-        this.fetchGeneration()
+        this.props.fetchGeneration()
 
-        let delay = new Date(this.state.generation.expiration).getTime() -
+        let delay = new Date(this.props.generation.expiration).getTime() -
             new Date().getTime()
 
         if(delay < MINIMUM_DELAY) delay = MINIMUM_DELAY
@@ -41,7 +27,7 @@ class Generation extends Component{
      }
 
     render(){
-        const { generation } = this.state;
+        const { generation } = this.props;
 
         return (
             <div>
@@ -58,6 +44,9 @@ const mapStateToProps = state => {
     return { generation }
 }
 
-const componentConnector = connect(mapStateToProps)
+const componentConnector = connect(
+    mapStateToProps, 
+    { fetchGeneration }
+)
 
 export default componentConnector(Generation)
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import {render} from 'react-dom';
+import thunk from 'redux-thunk'
 import Generation from './components/Generation'
 import Dragon from './components/Dragon'
 import Header from './components/Header'
-import { generationReducer } from './reducers'
-import { generationActionCreator } from './actions/generation'
+import rootReducer from './reducers'
 import './index.css'
 
 const store = createStore(
-    generationReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(thunk)
 )
-
-store.subscribe(() => console.log('store state update', store.getState()))
-
-fetch('http://localhost:5000/generation')
- .then(response => response.json())
-   .then(json => {
-       store.dispatch(generationActionCreator(json.generation))
-   })
 
 render(
     <Provider store={store}>
