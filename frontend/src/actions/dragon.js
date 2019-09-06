8a0f330abc603c3163785e0ab5f788c2e7ce5a83
Mahad Ansar <46062897+mahadansar@users.noreply.github.com>
2019-09-06 22:40:07 +0500

    redux dragon 1.0

---
+++ frontend/src/actions/dragon.js
@@ -0,0 +1,26 @@
+import { DRAGON } from './types'
+
+export const fetchDragon = () => dispatch => {
+    dispatch({ type: DRAGON.FETCH })
+
+    return fetch('http://localhost:5000/dragon/new')
+     .then(response => response.json())
+      .then(json => {
+          if(json.type === 'error'){
+              dispatch({
+                  type: DRAGON.FETCH_ERROR,
+                  message: json.message
+              })
+          }
+          else{
+            dispatch({ 
+                type: DRAGON.FETCH_SUCCESS,
+                dragon: json.dragon
+          })
+        }
+      })
+      .catch(error => dispatch({ 
+          type: DRAGON.FETCH_ERROR, 
+          message: error.message
+        }))
+}
