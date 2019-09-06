dde2ce27508901832c9313a8add17358a6dade1c
Mahad Ansar <46062897+mahadansar@users.noreply.github.com>
2019-09-07 00:28:24 +0500

    sha256 password hashing

--- backend/app/index.js
+++ backend/app/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 const GenerationEnginer = require('./generation/engine')
 const dragonRouter = require('./api/dragon')
 const generationRouter = require('./api/generation')
+const accountRouter = require('./api/account')
 
 const app = express()
 const engine = new GenerationEnginer()
@@ -10,6 +12,9 @@ const engine = new GenerationEnginer()
 app.locals.engine = engine
 
 app.use(cors({ origin: 'http://localhost:1234' }))
+app.use(bodyParser.json())
+
+app.use('/account', accountRouter)
 app.use('/dragon', dragonRouter)
 app.use('/generation', generationRouter)
 
--- backend/bin/configure_db.sh
+++ backend/bin/configure_db.sh
@@ -7,11 +7,17 @@ psql
 DROP DATABASE dragonstackdb;
 CREATE DATABASE dragonstackdb;
 
-CREATE TABLE account(id SERIAL PRIMARY KEY, username CHARACTER(64), password CHARACTER(64));
+
+CREATE TABLE account(id SERIAL PRIMARY KEY, "usernameHash" CHARACTER(64), "passwordHash" CHARACTER(64));
+
 CREATE TABLE generation ( id SERIAL PRIMARY KEY, expiration TIMESTAMP NOT NULL );
+
 CREATE TABLE dragon( id SERIAL PRIMARY KEY, birthdate TIMESTAMP NOT NULL, nickname VARCHAR(64), "generationId" INTEGER, FOREIGN KEY ("generationId") REFERENCES generation(id) );
+
 CREATE TABLE trait(id SERIAL PRIMARY KEY, "traitType" VARCHAR NOT NULL, "traitValue" VARCHAR NOT NULL);
+
 CREATE TABLE dragonTrait("traitId" INTEGER, "dragonId" INTEGER, FOREIGN KEY ("traitId") REFERENCES trait(id), FOREIGN KEY ("dragonId") REFERENCES dragon(id) );
+
 
 node ./bin/insertTraits.js
 
