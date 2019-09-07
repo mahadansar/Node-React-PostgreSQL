75aa908636d0d62fe0a6e824e745eed91b542855
Mahad Ansar <46062897+mahadansar@users.noreply.github.com>
2019-09-07 21:00:46 +0500

    login session to support new frontends

--- backend/app/api/account.js
+++ backend/app/api/account.js
@@ -35,7 +35,10 @@ router.post('./login', (req, res, next) => {
     AccountTable.getAccount({ usernameHash: hash(usernameHash) })
      .then(({ account }) => {
          if (account && account.passwordHash === hash(password)) {
-             return setSession({ username, res })
+             
+            const {sessonId} = account
+
+             return setSession({ username, res, sessonId })
          }else{
              const error = new Error('Incorrect username/password')
 
--- backend/app/api/helper.js
+++ backend/app/api/helper.js
@@ -2,27 +2,42 @@ const Session = require('../account/session')
 const AccountTable = require('../account/table')
 const {hash} = require('../account/helper')
 
-const setSession = ({username, res}) => {
+const setSession = ({username, res, sessionId}) => {
     return new Promise((resolve, reject) => {
 
-        const session = new Session({username})
-        const sessionString = session.toString()
+        let session, sessionString
 
-        AccountTable.updateSession({
-            sessionId: sessionId , 
-            usernameHash: hash(username)
-            })
-            .then(() => {
-                res.cookie('sessionString', sessionString, {
-                    expire: Date.now() + 3600000,
-                    httpOnly: true  //prevent javascript from stealing cookie(browser functionality)
-                    //secure: true //must use with https
-                 })
+        if(session) {
+            sessionString = Session.sessionString({ username, id: sessionId })
+            
+            setSessionCookie({ sessionString, res })
 
-                 resolve({ message: 'Session Created' })
-            })
-            .catch(error => reject(error))
+            resolve({message: 'Session Restored'})
+        }
+        else {
+            session = new Session({username})
+            sessionString = session.toString()
+
+            AccountTable.updateSession({
+                sessionId: sessionId , 
+                usernameHash: hash(username)
+                })
+                .then(() => {
+                    setSessionCookie({ sessionString, res })
+
+                     resolve({ message: 'Session Created' })
+                })
+                .catch(error => reject(error))
+        }
     })
+}
+
+const setSessionCookie = ({ sessionString, res}) => {
+    res.cookie('sessionString', sessionString, {
+        expire: Date.now() + 3600000,
+        httpOnly: true  //prevent javascript from stealing cookie(browser functionality)
+        //secure: true //must use with https
+     })
 }
 
 module.exports = { setSession }
